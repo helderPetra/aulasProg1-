c14620cad0a8f9fbcd8a4fe64227f232293a4a9d
helderp <hpetra73@gmail.com>
2015-10-21 00:00:58 +0000

    ola github!

---
+++ aula3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+int num= 0;
+    printf("os numeros pares:");
+    while(num<=50){
+        printf("%d-", num);
+        num= num+2;
+        
+    }
+    return 0;
+    }
